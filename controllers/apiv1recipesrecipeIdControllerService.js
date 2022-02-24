a7ac1691f6c549952e02497e048bee589659daac
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2022-02-24 20:49:31 +0100

    Initial code

---
+++ controllers/apiv1recipesrecipeIdControllerService.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports.findRecipeByrecipeId = function findRecipeByrecipeId(req, res, next) {
+  res.send({
+    recipeId: "g",
+    description: "d",
+    ingredients: "i",
+    complexity: 1
+  });
+};
+
+module.exports.deleteRecipe = function deleteRecipe(req, res, next) {
+  res.send({});
+};
+
+module.exports.updateRecipe = function updateRecipe(req, res, next) {
+  res.send({});
+};
